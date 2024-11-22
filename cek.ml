7618735b0ede2b7a7c44d65e1ab714d6191f624b
YudaiUrabe <yudaiurabe@gmail.com>
2024-11-22 22:34:00 +0900

    test

---
+++ cek.ml
@@ -0,0 +1,22 @@
+(* CEK Machine *)
+
+(* Syntax 
+cf. [Hillerström, ’17, slide p.13] 
+*)
+
+
+
+(* Control
+an expression currently being evaluated *)
+
+
+
+(* Environment
+a map  that contains the addresses of all variables in the lexical scope *)
+
+
+
+(* Continuation
+program’s execution context *)
+
+
