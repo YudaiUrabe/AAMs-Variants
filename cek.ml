615d5d6f67e15ad86f712050a4be176602bca050
YudaiUrabe <yudaiurabe@gmail.com>
2024-11-22 22:54:03 +0900

    test

--- cek.ml
+++ cek.ml
@@ -1,22 +1,12 @@
-(* CEK Machine *)
+(* Object Langugae -untyped lambda calculus- *)
 
+
+
+(* CEK Machine *)
 (* Syntax 
 cf. [Hillerström, ’17, slide p.13] 
 *)
 
 
-
-(* Control
-an expression currently being evaluated *)
-
-
-
-(* Environment
-a map  that contains the addresses of all variables in the lexical scope *)
-
-
-
-(* Continuation
-program’s execution context *)
 
 
