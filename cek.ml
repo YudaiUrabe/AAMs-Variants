df50cea5eafeeec123b151da042ae9e6a0189a87
YudaiUrabe <yudaiurabe@gmail.com>
2024-11-24 01:58:51 +0900

    temp

--- cek.ml
+++ cek.ml
@@ -1,12 +1,67 @@
-(* Object Langugae -untyped lambda calculus- *)
+(* Object Langugae -untyped lambda calculus(CbV)- *)
+
+type var = string
+and Lambda = var * term
+
+and term = 
+  | TmVar of var
+  | TmAbs of Lambda
+  | TmApp of term * term
+
+(* SYNTAX of CEK machine *)
+
+(* configuration_ triple 
+ (Control string
+an expression currently being evaluated)
+*)
+and Σ = term * Env * Cont
 
 
+and d = Clo of Lambda * Env
 
-(* CEK Machine *)
-(* Syntax 
-cf. [Hillerström, ’17, slide p.13] 
+(* Environment
+a map that contains the addresses of all variables in the lexical scope *)
+
+(* and Env = TmVar -> val *)
+
+(* Continuation
+evaluation context
 *)
 
+type Cont = 
+  | Done
+  | Ar of term * Env * Cont
+  | Fn of Lambda * Env * Cont
 
 
 
+
+
+
+(* (type operator) *)
+
+
+
+(* syntactic sugar *)
+
+
+
+(* transition relation as a partial function *)
+
+
+
+(* injection function *)
+
+
+
+(* Continuation
+program’s execution context *)
+
+
+
+(* evaluation function *)
+
+    (* auxiliary function *)
+
+
+(* isFinal *)
