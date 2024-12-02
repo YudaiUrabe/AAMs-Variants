d589229fb15077e75cb2f9985e967d74a5a9978a
占部雄大 <yudaiurabe2@urabeyuudais-MacBook-Air.local>
2024-12-02 15:50:24 +0900

    temp

--- cesk*.ml
+++ cesk*.ml
@@ -1,7 +1,15 @@
-(* Reference: "Systematic abstraction of abstract machines" *)
+(* Reference: "Systematic abstraction of abstract machines" §2.6 *)
+
+module StringMap = Map.Make(String)
 
 (* Object Langugae -untyped lambda calculus(CbV)- *)
 
+type var = string
+and lambda = var * term
+and term = 
+  | TmVar of var
+  | TmAbs of lambda
+  | TmApp of term * term
 
 
 
@@ -15,27 +23,64 @@ eliminate recursion from continuation in CESK machine
 (* configuration*)
 type config = term * env * store * cont
 
-(* Address *)
-and addr = int
-
-
-(* Environment *)
-and env = (var * addr) list
-
-
-(* Closure *)
-and storable = 
-  | Clo of lambda * env
-  | Cont of cont
-
-(* store is a finite map from address to storable values *)
-and store =  (addr * storable) list
-
-(* Continuation*)
+(* Continuation *)
 and cont = 
   | Done
   | Ar of term * env * addr
   | Fn of lambda * env * addr
 
+and storable = 
+  | Clo of lambda * env
+  | Cont of cont
+
+(* Environment *)
+and env = addr StringMap.t
+
+(* store *)
+ and store =  storable StringMap.t
 
 
+(* Address *)
+and addr = int
+
+(* tests *)
+
+
+(* type operator *)
+type map = string StringMap.t
+
+(* syntactic sugar *)
+let (==>) x y = (x, y)  (* tuple *)
+let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
+
+
+
+
+(* SEMANTICS of CESK* machine *)
+
+(* transition relation for the CESK machine *)
+let step (sigma: config): config = 
+  match sigma with
+  | (TmVar x, rho, sigma, kappa) ->
+    (* I will fix here soon! *)
+    let Clo(lam, rho') = List.assoc x rho in (TmAbs lam, rho', sigma, kappa)
+
+| (TmApp (f,e), rho, sigma, kappa) ->
+    (f, rho, sigma', kappa')
+    (* I will add some here. *)
+
+| (TmAbs lam, rho, sigma, Ar(e, rho', a')) ->
+    (e, rho', sigma, Fn(lam, rho, a')) 
+
+| (TmAbs lam, rho, sigma, Fn((x, e) , rho', a)) -> 
+    (e,rho'//[x ==> Clo(lam, rho)], kappa)   (* I will fix here soon. *)
+
+| _ -> failwith "Invalid configuration"
+
+(* alloc function *)
+let alloc (sigma: store): addr =
+~~~~~~
+
+
+
+(*　test *)
--- cesk.ml
+++ cesk.ml
@@ -1,11 +1,16 @@
-(* Reference: "Systematic abstraction of abstract machines" *)
+(* Reference: "Systematic abstraction of abstract machines" §2.4*)
 
+module StringMap = Map.Make(String)
 
-
+type var = string
+and lambda = var * term
+and term = 
+  | TmVar of var
+  | TmAbs of lambda
+  | TmApp of term * term
 
 (* CESK Machine *)
-(* add a store to CEK machine and use it allocate variable bindings, thereby eliminating recursion from the environment in CEK machine*)
-
+(* Adding a store to CEK machine and use it allocate variable bindings, thereby eliminating recursion from the environment in CEK machine*)
 
 (* SYNTAX of CESK machine *)
 
@@ -13,14 +18,15 @@
 type config = term * env * store * cont
 
 (* Environment *)
-and env = (var * addr) list
-
-(* Closure *)
+and env = addr StringMap.t
+  
 and storable = Clo of lambda * env
+(* 閉包はないのかしら，いや呼称しないだけかもで，こいつがCEKにおける閉包の代わりな気がする *)
 
 (* store
  is a finite map from address to storable values *)
- and store =  (addr * storable) list
+ and store =  storable StringMap.t
+(* これってちゃんとアドレスからのマップだと書けているか？ *)
 
 (* Continuation *)
 and cont = 
@@ -31,14 +37,53 @@ and cont =
 (* Address *)
 and addr = int
 
-
 (* tests *)
-let ex_env : env = [("a", 2)]
 
-let ex_term : term = TmApp(TmVar "a", TmVar "b")
 
-let test_storable : storable = Clo (("c", TmVar "c"), ex_env)
+(* type operator *)
+type map = string StringMap.t
 
-let test_store : store = [(3, Clo (("d", TmVar "d"),[]))]
+(* syntactic sugar *)
+let (==>) x y = (x, y)  (* tuple *)
+let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
 
-let test_cont : cont = Fn (("c", TmVar "c"), ex_env, Done)
+
+
+(* SEMANTICS of CESK machine *)
+
+(* transition relation for the CESK machine *)
+let step (sigma: config): config = 
+  match sigma with
+  | (TmVar x, rho, sigma, kappa) ->
+    (* I will fix here soon! *)
+    let Clo(lam, rho') = List.assoc x rho in (TmAbs lam, rho', sigma, kappa)
+
+| (TmApp (f,e), rho, sigma, kappa) ->
+    (f, rho, sigma, Ar(e, rho, kappa))
+
+| (TmAbs lam, rho, sigma, Ar(e, rho', kappa)) ->
+    (e, rho', sigma, Fn(lam, rho, kappa)) 
+(* 引数を評価するため継続を展開 *)
+
+| (TmAbs lam, rho, sigma, Fn((x, e) , rho', kappa)) -> 
+    (e,rho'//[x ==> Clo(lam, rho)], kappa)   (* I will fix here soon. *)
+| _ -> failwith "Invalid configuration"
+
+(* alloc function *)
+let alloc (sigma: store): addr =
+~~~~~~
+
+(* injection function *)
+let inject (e:term) : config =
+  (e, rho0, sigma0, Done)
+~~~~~~
+
+(* isFinal *)
+let isFinal (sigma_state: config) : bool =
+  match sigma_state with
+    |(TmAbs _, _, _, Done) -> true
+    | _ -> false
+
+(*　test *)
+
+
