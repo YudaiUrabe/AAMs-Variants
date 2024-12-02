bae2ce79fcdc34132d35cb05fdd7707987d6fd9d
占部雄大 <yudaiurabe2@urabeyuudais-MacBook-Air.local>
2024-12-02 15:46:36 +0900

    temp

--- time-stamped CESK*.ml
+++ time-stamped CESK*.ml
@@ -1,4 +1,14 @@
+(* Reference: "Systematic abstraction of abstract machines" §2.8 *)
+
+module StringMap = Map.Make(String)
+
 (* Object Langugae -untyped lambda calculus(CbV)- *)
+type var = string
+and lambda = var * term
+and term = 
+  | TmVar of var
+  | TmAbs of lambda
+  | TmApp of term * term
 
 
 
@@ -9,35 +19,78 @@
 (* configuration*)
 type config = term * env * store * cont * time
 
+and storable = 
+  | Clo of lambda * env
+  | Cont of cont
+
+
+(* Environment *)
+and env = addr StringMap.t
+
+(* store *)
+and store =  storable StringMap.t
+
+
+(* Continuation *)
+and cont = 
+  | Done
+  | Ar of term * env * addr
+  | Fn of lambda * env * addr
+
 (* Address *)
 and addr = int
 
 (* Time *)
 and time = int
 
-(* Environment *)
-and env = (var * addr) list
+(* tests *)
 
-(* Closure *)
-and storable = 
-  | Clo of lambda * env
-  | Cont of cont
 
-(* store is a finite map from address to storable values *)
-and store =  (addr * storable) list
+(* type operator *)
+type map = string StringMap.t
 
-(* Continuation*)
-and cont = 
-  | Done
-  | Ar of term * env * addr
-  | Fn of lambda * env * addr
+(* syntactic sugar *)
+let (==>) x y = (x, y)  (* tuple *)
+let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
 
 
 
 
-  (* tick function *)
 
 
-  (* alloc function *)
+(* SEMANTICS of time-stamped CESK* machine *)
+
+(* alloc function *)
+let alloc (sigma: config): addr =
+  ~~~~~~
+  
+(* tick function *)
+let tick (sigma: config): time =
+  ~~~~~~
+
+(* transition relation for the time-stamped CESK* machine *)
+
+(* I need to fix all patterns *)
+
+let step (sigma: config): config = 
+  match sigma with
+  | (TmVar x, rho, sigma, kappa, t) ->
+    (* I will fix here soon! *)
+    let Clo(lam, rho') = List.assoc x rho in (TmAbs lam, rho', sigma, kappa, t')
+
+| (TmApp (f,e), rho, sigma, kappa, t) ->
+    (f, rho, sigma', kappa', t')
+    (* I will add some here. *)
+
+| (TmAbs lam, rho, sigma, Ar(e, rho', a', t)) ->
+    (e, rho', sigma, Fn(lam, rho, a', t')) 
+
+| (TmAbs lam, rho, sigma, Fn((x, e) , rho', a), t) -> 
+    (e,rho'//[x ==> Clo(lam, rho)], kappa, t')   (* I will fix here soon. *)
+
+| _ -> failwith "Invalid configuration"
+
+
+
 
 
