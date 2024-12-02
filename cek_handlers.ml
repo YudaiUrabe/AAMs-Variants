9782dcc5f4c005696faaaf4aa33428684493adad
占部雄大 <yudaiurabe2@urabeyuudais-MacBook-Air.local>
2024-12-02 15:48:46 +0900

    temp

--- cek_handlers.ml
+++ cek_handlers.ml
@@ -1,12 +1,16 @@
 (* Object Langugae -fine-graing CbV lambda calculus- *)
 (* Reference: "Liberating Effects with Rows and Handlers" *)
 
+module StringMap = Map.Make(String)
+
+(* Object Langugae -Fine-grain call-by-value lambda calculus- *)
 type var = string
 and lambda = var * term
 and term = 
   | TmVar of var
   | TmAbs of lambda
 
+~~~~~
 (* computations *)
 
 (* handlers *)
@@ -14,41 +18,91 @@ and term =
 
 (* SYNTAX of CEK machine with handlers *)
 
-(* configuration*)
-(* type config = term * env * cont *)
-
-(* Environment *)
-(* and env = (var * addr) list *)
+(* configuration
+*)
+type config = term * env * cont
 
 
-(* Closure *)
-(* and d = Clo of env * handler)
+
+(* Environment
+*)
+and env = d StringMap.t
 
 (* Continuation*)
-(* and cont = 
+
+and cont = 
   | Done (* hole *)
   | Ar of term * env * cont
-  | Fn of lambda * env * cont *)
+  | Fn of lambda * env * cont
+~~~~~~
+(* Hanlder Closures　*)
 
+
+
+
+  (* type operator *)
+(* type map = string StringMap.t *)
+
+(* syntactic sugar *)
+(* 
+let (==>) x y = (x, y)  (* tuple *)
+let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
+ *)
 
 
 
 
 (* SEMANTICS of this machine *)
 
-
-
-(* Identity continuation *)
-
-(* transition relation 
+(* transition function 
 M-INT, M-APP, M-APPCONT, 
 M-LET,M-HANDLER,
 M-RETCONT, M-RETHANDLER, M-RETTOP,
 M-OP, M-OP-HANDLER, M-OP-FORWARD 
 *)
 
+(* Identity continuation *)
+
+(*
+let step (sigma: config): config = 
+  match sigma with
+  | (TmVar x, rho, kappa) ->
+    let Clo(lam, rho') = List.assoc x rho in (TmAbs lam, rho', kappa)
+  | (TmApp (f,e), rho, kappa) ->
+    (f, rho, Ar(e, rho, kappa))
+  | (TmAbs lam, rho, Ar(e, rho', kappa)) ->
+    (e, rho', Fn(lam, rho, kappa)) 
+  | (TmAbs lam, rho, Fn((x, e) , rho', kappa)) -> 
+     (e,rho'//[x ==> Clo(lam, rho)], kappa)
+  | _ -> failwith "Invalid configuration"
+*)
+
+(*
+
+(* injection function 
+the initial machine state for a closed expression e *)
+let inject (e:term) : config =
+  (e, StringMap.empty, Done)
+
+(* auxiliary functions for evaluation function *)
+(* isFinal *)
+let isFinal (sigma_state: config) : bool =
+  match sigma_state with
+    |(TmAbs _, rho, Done) -> true
+    | _ -> false
+
+(* collect *)
+let rec collect (f: config -> config) (isFinal: config-> bool)(sigma_collect: config): config list =
+  if isFinal sigma_collect then
+    [sigma_collect]
+  else
+    sigma_collect :: collect f isFinal (f sigma_collect)
+
+(* evaluation function *)
+let evaluate (e: term): config list =
+  collect step isFinal(inject e)
+
+(*　test *)
 
 
-
-(* Value interpretation *)
-
+*)
