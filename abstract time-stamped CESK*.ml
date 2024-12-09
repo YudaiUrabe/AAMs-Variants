a9d6904b83cadec4b3ad43df905d8c9dc5abdc97
YudaiUrabe <yudaiurabe@gmail.com>
2024-12-09 11:30:43 +0900

    temp

--- abstract time-stamped CESK*.ml
+++ abstract time-stamped CESK*.ml
@@ -1,53 +1,95 @@
 (* Object Langugae -untyped lambda calculus(CbV)- *)
 
+module StringMap = Map.Make(String)
+module AddrMap = Map.Make(Int)
+
+(* Object Langugae -untyped lambda calculus(CbV)- *)
+type var = string
+and lambda = var * term
+and term = 
+  | TmVar of var
+  | TmAbs of lambda
+  | TmApp of term * term
 
 
-(* 
-goal: to construct a function, CEK_hat
-
-abstract state: Σ_hat
-abstract state transition: |->CEK_hat
-abstraction map: α: Σ →  Σ_hat 
-abstract evaluation function: CEK_hat(e)
-
-*)
-
-
-
-
-
-(* abstract time-stamped CESK* 
-*)
+(* abstract time-stamped CESK* *)
 
 (* SYNTAX of abstract time-stamped CESK* machine *)
 
 (* configuration*)
 type config = term * env * store * cont * time
 
-(* Address *)
-(* and addr = int *)
-
-(* Time *)
-(* and time = int *)
-
-(* Environment *)
-(* and env = (var * addr) list *)
-
-(* Closure *)
 and storable = 
   | Clo of lambda * env
   | Cont of cont
 
-(* store is a finite map from address to storable values *)
-and store =  (addr * storable) list
+(* Environment *)
+and env = addr StringMap.t
 
-(* Continuation*)
+(* store *)
+and store =  storable AddrMap.t
+
+
+(* Continuation *)
 and cont = 
   | Done
   | Ar of term * env * addr
   | Fn of lambda * env * addr
 
+(* Time *)
+and time = int
 
+
+(* Address *)
+and addr = int
 
 
   (* type P *)
+
+
+
+(* tests *)
+
+
+
+
+(* type operator *)
+type map = string StringMap.t
+
+(* syntactic sugar *)
+let (==>) x y = (x, y)  (* tuple *)
+let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
+
+
+
+(* Lattice *)
+
+
+
+
+
+(* transition relation *)
+let step (sigma: config): ~~~~ = 
+  match sigma with
+  | (TmVar x, rho, s, kappa, t) ->
+
+  | (TmApp (f,e), rho, s, kappa, t) ->
+
+  | (TmAbs lam, rho, s, Ar(e, rho', a'), t) ->
+
+  | (TmAbs lam, rho, s, Fn((x, e) , rho', a), t) -> 
+
+  | _ -> failwith "Invalid configuration"
+
+
+
+
+(* aval *)
+
+(* explore *)
+
+(* ∈ *)
+
+(* search *)
+
+
