d41ac4245f936aeced45fa3815e5815c50cd90b4
YudaiUrabe <yudaiurabe@gmail.com>
2024-12-31 23:36:37 +0900

    done

--- cek.ml
+++ cek.ml
@@ -1,10 +1,10 @@
-(* Reference: "Systematic abstraction of abstract machines" §2.2, TAPL §7*)
+(* Reference: "Systematic abstraction of abstract machines" §2.2, TAPL §7 *)
 
 module StringMap = Map.Make(String)
 
 (* Object Langugae -untyped lambda calculus(CbV)- *)
 type var = string
-and lambda = var * term
+and lambda = var * term (* value *)
 and term = 
   | TmVar of var
   | TmAbs of lambda
@@ -22,7 +22,6 @@ type config = term * env * cont
  is pair of a value and environment
 *)
 and d = Clo of lambda * env
-
 
 (* Environment
 is implemented as finite maps from variables to closures.
@@ -47,28 +46,23 @@ let (==>) x y = (x, y)  (* tuple *)
 let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key value acc) map entries
 
 
+
+
 (* SEMANTICS of CEK machine *)
 
-(* transition relation for the CEK machine 
-as a partial function 
-one-step
+(* (one-step) transition relation for the CEK machine 
 *)
-
-
 let step (sigma: config): config = 
   match sigma with
   | (TmVar x, rho, kappa) ->
       let Clo(lam, rho') = StringMap.find x rho in (TmAbs lam, rho', kappa)
-
   | (TmApp (f,e), rho, kappa) ->
       (f, rho, Ar(e, rho, kappa))
-
   | (TmAbs lam, rho, Ar(e, rho', kappa)) ->
       (e, rho', Fn(lam, rho, kappa)) 
-
-  | (TmAbs lam, rho, Fn((x, e) , rho', kappa)) -> 
+  | (TmAbs lam, rho, Fn((x, e) , rho', kappa)) ->
       (e,rho'//[x ==> Clo(lam, rho)], kappa)
- | _ ->
+  | _ ->
       failwith "Invalid configuration"
 
 (* injection function 
@@ -79,7 +73,7 @@ let inject (e:term) : config =
 (* auxiliary functions for evaluation function *)
 (* isFinal 
 A state is final when it has no next step.
-So, this function checks if the continuation is empty.
+This function checks if the continuation is empty.
 *)
 let isFinal (sigma_state: config) : bool =
   match sigma_state with
@@ -89,10 +83,9 @@ let isFinal (sigma_state: config) : bool =
 (* collect *)
 let rec collect (f: config -> config) (isFinal: config-> bool)(sigma_collect: config): config list =
   if isFinal sigma_collect then
-    [sigma_collect] 
+    [sigma_collect]
   else
     sigma_collect :: collect f isFinal (f sigma_collect)
-
 
 (* evaluation function *)
 (* Create an initial state from the term e using "inject",
@@ -102,11 +95,12 @@ let evaluate (e: term): config list =
 
 
 
+
+
 (*　test 
 (λa.a)(λb.b) -> (λb.b)
 *)
 let term_test = TmApp (TmAbs ("a", TmVar "a"), TmAbs ("b", TmVar "b"))
-
 
 let result = evaluate term_test
 
@@ -116,3 +110,9 @@ let rec string_of_term (t: term) =
   | TmVar x -> x
   | TmAbs (x, t) -> "λ" ^ x ^ "." ^ string_of_term t
   | TmApp (t1, t2) -> "(" ^ string_of_term t1 ^ " " ^ string_of_term t2 ^ ")"
+
+(* output *)
+let () = 
+  List.iter (fun (term_test, _, _) -> 
+    Printf.printf "State: %s\n" (string_of_term term_test)
+  ) result
