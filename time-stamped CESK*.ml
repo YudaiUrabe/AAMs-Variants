ed4ef8b5750c56e96ca4654d1979780f0c15842d
YudaiUrabe <yudaiurabe@gmail.com>
2025-01-29 19:25:28 +0900

    temp

--- time-stamped CESK*.ml
+++ time-stamped CESK*.ml
@@ -12,8 +12,7 @@ and term =
   | TmApp of term * term
 
 (* time-stamped CESK* machine *)
-今回は，time-stamp machine stateみたい
-なんでtimeなんて入れたんだ〜〜〜〜〜？
+
 
 
 (* SYNTAX of time-stamped CESK* machine *)
@@ -24,7 +23,7 @@ type config = term * env * store * cont * time
 and storable = 
   | Clo of lambda * env
   | Cont of cont
-(* 一つ前のやつでこう書いている；この最後の行ってOCamlのコードとして正しいかしら． 少し気になっただけかもだが，Cloってこんなふうにofで定義してたっけ．*)
+
 
 (* Environment *)
 and env = addr StringMap.t
@@ -46,8 +45,6 @@ and time = int
 
 
 
-
-(* tests *)
 
 
 
@@ -72,9 +69,7 @@ let alloc (sigma: config): addr =
     let keys = AddrMap.fold (fun key _ acc -> key :: acc) s [] in
     let max_key = List.fold_left max 0 keys in
     max_key + 1
-ここの実装自信がない, configようにちゃんと実装せんと
-  (* sから全てのキーを取り出して，そのリストから最大のキーを求め，その最大のキーに１を加えたものをアドレスとして返す *)
-  コレクトやevalの代わりかしら？
+
   
 
 
@@ -82,46 +77,7 @@ let alloc (sigma: config): addr =
 let tick (sigma: config): time =
   let (_, _, _, _, t) = sigma in
   t + 1
-(* ちょい自信がない．．． *)
-
-
-  
-
 
 
 
 (* transition relation for the time-stamped CESK* machine *)
-
-let step (sigma: config): config = 
-  match sigma with
-  | (TmVar x, rho, s, kappa, t) ->
-    let Clo(lam, rho') =  StringMap.find x rho in
-        (* CESKと同じだが，ここって，String Mapでいいんかいな？あと，Clo＝の右辺のやつ絶対うまく実装できていない気がする．．．StringMap.find x rho でええのか？ *)
-    let t' = tick sigma in
-    (* これ ς@はシグマで代用して本当にいいの？？？*)
-      (TmAbs lam, rho', s, kappa, t')
-
-  | (TmApp (f,e), rho, s, kappa, t) ->
-    let a' = alloc sigma in
-    (* ここもς@はシグマで代用して本当にいいの？ *)
-    let s' = s//[a' ==> Cont kappa] in
-    let kappa' = Ar(e, rho, a') in
-    let t' = tick sigma in
-      (f, rho, s', kappa', t')
-
-  | (TmAbs lam, rho, s, Ar(e, rho', a'), t) ->
-    let t' = tick sigma in
-      (e, rho', s, Fn(lam, rho, a'), t') 
-
-  | (TmAbs lam, rho, s, Fn((x, e) , rho', a), t) -> 
-    let Cont kappa = StringMap.find a s in
-    let a' = alloc sigma in
-    let t' = tick sigma in
-      (e, rho'//[x ==> a'], s//[a' ==> Clo(lam, rho)], kappa, t')
-
-  | _ -> failwith "Invalid configuration"
-
-
-(* ほんまに，実装の際に，ς@を無視してしまっていいのか？このステップ関数の，シグマで置き換えてるところを全部見直すべきかも *)
-
-
