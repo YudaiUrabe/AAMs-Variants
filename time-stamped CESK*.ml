377dac59a9e3ee72fb994572c8b2dbd8486a49fc
YudaiUrabe <yudaiurabe@gmail.com>
2025-01-29 18:50:29 +0900

    WIP: Save progress before rebase

--- time-stamped CESK*.ml
+++ time-stamped CESK*.ml
@@ -12,6 +12,9 @@ and term =
   | TmApp of term * term
 
 (* time-stamped CESK* machine *)
+今回は，time-stamp machine stateみたい
+なんでtimeなんて入れたんだ〜〜〜〜〜？
+
 
 (* SYNTAX of time-stamped CESK* machine *)
 
@@ -21,6 +24,7 @@ type config = term * env * store * cont * time
 and storable = 
   | Clo of lambda * env
   | Cont of cont
+(* 一つ前のやつでこう書いている；この最後の行ってOCamlのコードとして正しいかしら． 少し気になっただけかもだが，Cloってこんなふうにofで定義してたっけ．*)
 
 (* Environment *)
 and env = addr StringMap.t
@@ -64,7 +68,13 @@ let (//) map entries = List.fold_left(fun acc(key, value) -> StringMap.add key v
 
 (* alloc function *)
 let alloc (sigma: config): addr =
-
+    let (_, _, s, _, _) = sigma in
+    let keys = AddrMap.fold (fun key _ acc -> key :: acc) s [] in
+    let max_key = List.fold_left max 0 keys in
+    max_key + 1
+ここの実装自信がない, configようにちゃんと実装せんと
+  (* sから全てのキーを取り出して，そのリストから最大のキーを求め，その最大のキーに１を加えたものをアドレスとして返す *)
+  コレクトやevalの代わりかしら？
   
 
 
@@ -72,7 +82,7 @@ let alloc (sigma: config): addr =
 let tick (sigma: config): time =
   let (_, _, _, _, t) = sigma in
   t + 1
-
+(* ちょい自信がない．．． *)
 
 
   
@@ -86,11 +96,14 @@ let step (sigma: config): config =
   match sigma with
   | (TmVar x, rho, s, kappa, t) ->
     let Clo(lam, rho') =  StringMap.find x rho in
+        (* CESKと同じだが，ここって，String Mapでいいんかいな？あと，Clo＝の右辺のやつ絶対うまく実装できていない気がする．．．StringMap.find x rho でええのか？ *)
     let t' = tick sigma in
+    (* これ ς@はシグマで代用して本当にいいの？？？*)
       (TmAbs lam, rho', s, kappa, t')
 
   | (TmApp (f,e), rho, s, kappa, t) ->
     let a' = alloc sigma in
+    (* ここもς@はシグマで代用して本当にいいの？ *)
     let s' = s//[a' ==> Cont kappa] in
     let kappa' = Ar(e, rho, a') in
     let t' = tick sigma in
@@ -107,4 +120,8 @@ let step (sigma: config): config =
       (e, rho'//[x ==> a'], s//[a' ==> Clo(lam, rho)], kappa, t')
 
   | _ -> failwith "Invalid configuration"
+
+
+(* ほんまに，実装の際に，ς@を無視してしまっていいのか？このステップ関数の，シグマで置き換えてるところを全部見直すべきかも *)
+
 
