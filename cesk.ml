43ab9b0757f032a07a27913411d769abf221db33
YudaiUrabe <yudaiurabe@gmail.com>
2025-05-10 21:06:00 +0900

    Improve readability of alloc function

--- cesk.ml
+++ cesk.ml
@@ -38,8 +38,6 @@ and cont =
 and addr = int
 
 
-
-
 (* type operator *)
 type map = string StringMap.t
 
@@ -53,17 +51,14 @@ let (///) map entries = List.fold_left(fun acc(key, value) -> AddrMap.add key va
 
 
 
-
-
 (* SEMANTICS of CESK machine *)
 
 (* alloc function *)
-(* Extract all keys from s, find the maximum key in the list,
- and return the maximum key plus one as the new address. *)
  let alloc (s: store): addr =
-  let keys = AddrMap.fold (fun key _ acc -> key :: acc) s [] in
-  let max_key = List.fold_left max 0 keys in
-  max_key + 1
+  let keys = List.map fst (AddrMap.bindings s) in                (* Extract all keys from s,*)
+  (* let keys = AddrMap.fold (fun key _ acc -> key :: acc) s [] in  *)
+  let max_key = List.fold_left max 0 keys in                     (* find the maximum key in the list,*)
+  max_key + 1                                                    (* and return the maximum key plus one as the new address. *)
 
 (* transition relation for the CESK machine *)
 let step (sigma: config): config = 
@@ -86,6 +81,9 @@ combines the term with the empty env, store and continuation*)
 let inject (e:term) : config =
  (e, StringMap.empty, AddrMap.empty, Done)
 
+
+
+
 (* isFinal *)
 let isFinal (state: config) : bool =
   match state with
@@ -104,29 +102,70 @@ let evaluate (e: term): config list =
   collect step isFinal(inject e)
 
 
-(* test 
-(λa.a)(λb.b) -> (λb.b)　*)
-let term_test = TmApp (TmAbs ("a", TmVar "a"), TmAbs ("b", TmVar "b"))
 
-(* test2
-suc = λnsz.s(nsz)
-1 = λsz.sz
-suc 1 -> λsz.s(sz) = 2
- *)
- let term_test2 = TmApp(
-                    TmAbs("n", TmAbs ("s",TmAbs ("z", TmApp (TmVar "s", TmApp(TmApp(TmVar "n", TmVar "s"), TmVar "z"))))),
-                    TmAbs ("s", TmAbs ("z", TmApp (TmVar "s", TmVar "z"))))
 
-let result = evaluate term_test2
+
+
+
+
+
+
+
+(* tests *)
+(* auxiliary function for the tests *)
 
 let rec string_of_term (t: term) =
   match t with
   | TmVar x -> x
   | TmAbs (x, t) -> "λ" ^ x ^ "." ^ string_of_term t
   | TmApp (t1, t2) -> "(" ^ string_of_term t1 ^ " " ^ string_of_term t2 ^ ")"
+let rec string_of_env (env: env) =
+  let bindings = StringMap.bindings env in
+  let binding_to_string (var, Clo ((x, t), clo_env)) =
+      var ^ " ↦ (λ" ^ x ^ "." ^ string_of_term t ^ ", " ^ string_of_env clo_env ^ ")"
+    in
+    "{" ^ String.concat ", " (List.map binding_to_string bindings) ^ "}"    
+let rec string_of_store (store: store) =
+〜〜
+
+
+let rec string_of_cont (cont: cont) =
+  match cont with
+  | Done -> "Done"
+  | Ar (t, _, k) -> "Ar(" ^ string_of_term t ^ ", " ^ string_of_cont k ^ ")"
+  | Fn ((x, t), _, k) -> "Fn(λ" ^ x ^ "." ^ string_of_term t ^ ", " ^ string_of_cont k ^ ")"
+let string_of_state (t: term) (env: env) (store: store)(cont: cont) =
+  "{" ^
+   string_of_term t ^ ", " ^
+   string_of_env env ^ ", " ^
+   string_of_store store ^ ", " ^
+   string_of_cont cont ^
+  "}"  
+
+
+
+
+let term_test1 = TmApp (TmAbs ("a", TmVar "a"), TmAbs ("b", TmVar "b"))
+
+
+
+ let term_test2 = TmApp(
+                    TmAbs("n", TmAbs ("s",TmAbs ("z", TmApp (TmVar "s", TmApp(TmApp(TmVar "n", TmVar "s"), TmVar "z"))))),
+                    TmAbs ("s", TmAbs ("z", TmApp (TmVar "s", TmVar "z"))))
+
+
 
 (* output *)
 let () = 
   List.iter (fun (term_test, _, _, _) -> 
     Printf.printf "State: %s\n" (string_of_term term_test)
   ) result
+
+
+
+let () =
+let result1 = evaluate term_test1 in
+let result2 = evaluate term_test2 in
+print_trace "Test 1" result1;
+print_trace "Test 2" result2
+
